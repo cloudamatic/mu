f97a17f3e454661152b3add4cfa2d5d3efc3c71a
jaibapna <jaibapna>
2015-04-27 15:23:23 +0530

    Update YAML

--- site_cookbooks/demo/recipes/wordpress.rb
+++ site_cookbooks/demo/recipes/wordpress.rb
@@ -10,14 +10,41 @@
 
 case node[:platform_family]
 
+include_recipe "apache2"
+include_recipe "php"
+
+$git_repo_name=node['mu_wordpress']['global']['git_repo_name']
+$database=node['deployment']['databases']
+$loadbalancer=node['deployment']['loadbalancers']
+$lb_url=$loadbalancer['mu-wordpress-lb']['dns'].downcase
+$db_name=node['mu_wordpress']['global']['db_name']
+$db_host=$database['mu-wordpress-db']['endpoint']
+$db_user=$database['mu-wordpress-db']['username']
+$db_password=$database['mu-wordpress-db']['password']
+
 when "rhel"	
 
-
+execute "yum -y install php-mbstring" do
+action :run
+end
 
 
 
 
 when "debian"
+
+include_recipe "apache2::mod_php5"
+
+execute "apt-get -y install php-mbstring" do
+action :run
+end
+
+bash "Create mysql database in RDS" do
+	user "root"
+	code <<-EOH
+		mysql -h #{$db_host} -u #{$db_user} -p#{$db_password} -e "CREATE DATABASE IF NOT EXISTS #{$db_name};"
+	EOH
+end
 
 
 
