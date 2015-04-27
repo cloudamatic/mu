512d2aaa7db663539cd6288197975129832a4ad2
jaibapna <jaibapna>
2015-04-27 20:14:46 +0530

    Update YAML

--- site_cookbooks/demo/recipes/wordpress.rb
+++ site_cookbooks/demo/recipes/wordpress.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: mu_wordpress
+# Cookbook Name:: demo
 # Recipe:: wp-cli
 #
 # Copyright 2015, YOUR_COMPANY_NAME
@@ -12,32 +12,44 @@ case node[:platform_family]
 
 include_recipe "apache2"
 include_recipe "php"
+include_recipe "mysql"
+include_recipe "demo::apache"
 
 $git_repo_name=node['mu_wordpress']['global']['git_repo_name']
 $database=node['deployment']['databases']
 $loadbalancer=node['deployment']['loadbalancers']
-$lb_url=$loadbalancer['mu-wordpress-lb']['dns'].downcase
-$db_name=node['mu_wordpress']['global']['db_name']
-$db_host=$database['mu-wordpress-db']['endpoint']
-$db_user=$database['mu-wordpress-db']['username']
-$db_password=$database['mu-wordpress-db']['password']
+$lb_url=$loadbalancer['wordpress-demo-lb']['dns'].downcase
+$db_name="wordpress_db"
+$db_host=$database['wordpress-demo-db']['endpoint']
+$db_user=$database['wordpress-demo-db']['username']
+$db_password=$database['wordpress-demo-db']['password']
+$loadbalancer=node['deployment']['loadbalancers']
+$app_url=$loadbalancer['wordpress-demo-lb']['dns'].downcase
+$title="mu wordpress demo"
+$admin_user="admin"
+$admin_password="admin"
+$admin_email="admin@example.com"
+
 
 when "rhel"	
 
 execute "yum -y install php-mbstring" do
-action :run
+ action :run
+end
+
+execute "yum -y install php-mysql" do
+ action :run
+end
+
+execute "setsebool httpd_can_network_connect=1" do
+ action :run
 end
 
 
-
-
-when "debian"
-
-include_recipe "apache2::mod_php5"
-
-execute "apt-get -y install php-mbstring" do
-action :run
+execute "setsebool httpd_can_network_connect=1 | echo SELinux is disabled" do
+ action :run
 end
+
 
 bash "Create mysql database in RDS" do
 	user "root"
@@ -46,6 +58,55 @@ bash "Create mysql database in RDS" do
 	EOH
 end
 
+
+bash "Launch Wordpress site" do
+	user "root"
+	code <<-EOH
+		cd /var/www
+		mkdir -p wordpressapp
+		cd wordpressapp
+		wp core download
+		wp core config --dbname=#{$db_name} --dbuser=#{$db_user} --dbpass=#{$db_password} --dbhost=#{$db_host}
+		wp core multisite-install --url=#{$app_url} --title=#{$title} --admin_user=#{$admin_user}  --admin_password=#{$admin_password}  --admin_email=#{$admin_email}
+	EOH
+	not_if  {::File.exists?("/var/www/wordpressapp/wp-config.php") }
+end
+
+
+service "httpd" do
+  action :restart
+end
+
+when "debian"
+
+include_recipe "apache2::mod_php5"
+
+bash "Create mysql database in RDS" do
+	user "root"
+	code <<-EOH
+		mysql -h #{$db_host} -u #{$db_user} -p#{$db_password} -e "CREATE DATABASE IF NOT EXISTS #{$db_name};"
+	EOH
+end
+
+
+bash "Launch Wordpress site" do
+	user "root"
+	code <<-EOH
+		cd /var/www
+		mkdir -p wordpressapp
+		cd wordpressapp
+		wp core download
+		wp core config --dbname=#{$db_name} --dbuser=#{$db_user} --dbpass=#{$db_password} --dbhost=#{$db_host}
+		wp core multisite-install --url=#{$app_url} --title=#{$title} --admin_user=#{$admin_user}  --admin_password=#{$admin_password}  --admin_email=#{$admin_email}
+	EOH
+	not_if  {::File.exists?("/var/www/wordpressapp/wp-config.php") }
+end
+
+
+
+service "apache2" do
+  action :restart
+end
 
 
 else
