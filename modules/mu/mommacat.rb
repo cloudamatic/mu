fec75414de6256b0fdc01f2ca68454619008935f
John Stange <john.stange@eglobaltech.com>
2015-05-29 14:42:44 -0400

    gem autoload not working in some random place, work aroudn it

--- modules/mu/mommacat.rb
+++ modules/mu/mommacat.rb
@@ -1505,6 +1505,7 @@ MESSAGE_END
 					# fetch Chef data that the nodes have generated
 					sibling_collection.each_pair { |nodename, sibling|
 						begin
+							require 'chef'
 							chef_node = Chef::Node.load(nodename)
 						rescue Net::HTTPServerException => e
 							# This isn't typically an error condition. Usually happens when
