06397e171e7b53618e05dbb55d6e7608d06c888d
John Stange <john.stange@eglobaltech.com>
2015-06-08 14:12:40 -0400

    drop soon-to-be-useless Windows password generation code

--- modules/mu/resources/serverpool.rb
+++ modules/mu/resources/serverpool.rb
@@ -43,18 +43,6 @@ module MU
 			pool_name = MU::MommaCat.getResourceName(@pool['name'])
 			MU.setVar("curRegion", @pool['region']) if !@pool['region'].nil?
 
-
-			if @pool['platform'] == "windows"
-				if !@deploy.winpass.nil?
-					winpass = @deploy.winpass
-				elsif !@pool['never-generate_admin_password'] and !@pool['windows_admin_password']
-					winpass = MU::Server.generateWindowsAdminPassword
-				end
-				if !winpass.nil?
-					MU.mommacat.saveSecret("default", winpass, "windows_password")
-				end
-			end
-
 			asg_options = {
 				:auto_scaling_group_name => pool_name,
 				:default_cooldown => @pool["default_cooldown"],
