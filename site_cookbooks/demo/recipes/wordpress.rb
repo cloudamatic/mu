8d9ab5b77c8f15411506dde4252b46ff80fecc21
root <root@egt-labs-prod-mu-master.(none)>
2015-06-23 16:51:08 -0400

    working, cleanup pass

--- site_cookbooks/demo/recipes/wordpress.rb
+++ site_cookbooks/demo/recipes/wordpress.rb
@@ -1,21 +1,21 @@
 #
 # Cookbook Name:: demo
-# Recipe:: wp-cli
+# Recipe:: wp-web
 #
-# Copyright 2015, YOUR_COMPANY_NAME
+# Copyright 2014, eGlobalTech
 #
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "php"
-include_recipe "apache2"
-include_recipe "demo::mysql"
-include_recipe "demo::wp-cli"
-include_recipe "demo::apache"
-include_recipe "demo::iptables-port"
+include_recipe "apache2::mod_proxy"
+include_recipe "apache2::mod_proxy_http"
+include_recipe "apache2::mod_expires"
+include_recipe "apache2::mod_deflate"
+include_recipe "apache2::mod_ssl"
+include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_cgi"
 
-
-
+# Fetch important values from node
 $database=node['deployment']['databases']
 $loadbalancer=node['deployment']['loadbalancers']
 $lb_url=$loadbalancer['lb']['dns'].downcase
@@ -23,6 +23,7 @@ $db_name="wordpress_db"
 $db_host=$database['db']['endpoint']
 $db_user=$database['db']['username']
 $db_password=$database['db']['password']
+$db_endpoint=$database['db']['endpoint']
 $loadbalancer=node['deployment']['loadbalancers']
 $app_url=$loadbalancer['lb']['dns'].downcase
 $title="mu wordpress demo"
@@ -30,151 +31,174 @@ $admin_user="admin"
 $admin_password="admin"
 $admin_email="admin@example.com"
 
+puts $database, $loadbalancer
 
+package "php-mysql"
+package "mysql"
 
-case node[:platform_family]
-
-
-when "rhel"	
-
-	package "php-mbstring"
-
-	package "php-mysql"
-
-	execute "setsebool httpd_can_network_connect=1" do
-	 action :run
-	end
-
-
-	execute "setsebool httpd_can_network_connect=1 | echo SELinux is disabled" do
-	 action :run
-	end
-
-
-	bash "Create mysql database in RDS" do
-		user "root"
-		code <<-EOH
-			mysql -h #{$db_host} -u #{$db_user} -p#{$db_password} -e "CREATE DATABASE IF NOT EXISTS #{$db_name};"
-		EOH
-	end
-
-	
-	template '/etc/profile.d/wordpress.sh' do
-		owner 'root'
-		group 'root'
-		mode '0644'
-		source "apache/wordpress.sh.erb"
-		variables({
-				  :db_name => $db_name,
-				  :db_host => $db_host,
-				  :db_user => $db_user,
-				  :db_password => $db_password,
-				  :domain => $app_url
-				})
-	end
-
-
-	bash "Launch Wordpress site" do
-		user "root"
-		code <<-EOH
-			cd /var/www
-			mkdir -p wordpressapp
-			cd wordpressapp
-			wp core download
-			wp core config --dbname=#{$db_name} --dbuser=#{$db_user} --dbpass=#{$db_password} --dbhost=#{$db_host}
-			wp core multisite-install --url=#{$app_url} --title='#{$title}' --admin_user=#{$admin_user}  --admin_password=#{$admin_password}  --admin_email=#{$admin_email}
-		EOH
-		not_if  {::File.exists?("/var/www/wordpressapp/wp-config.php") }
-	end
-
-
-	template '/var/www/wordpressapp/heartbeat.php' do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source "wordpress/heartbeat.php.erb"
-	end
-
-
-	template '/var/www/wordpressapp/.htaccess' do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source "wordpress/htaccess.erb"
-	end
-
-
-	bash "Change owner and group" do
+# Create the db if brand new
+bash "Create mysql database in RDS" do
 	user "root"
-	code <<-EOH  
-		cd /var/www
-		chown -R apache:apache wordpressapp
+	code <<-EOH
+		mysql -h #{$db_host} -u #{$db_user} -p#{$db_password} -e "CREATE DATABASE IF NOT EXISTS #{$db_name};"
 	EOH
+end
+
+# The apache2 cookbook doesn't support mod_ext_filter for some reason, but it
+# comes with the package, so that's easy enough.
+link "/etc/httpd/mods-enabled/ext_filter.load" do
+	to "/etc/httpd/mods-available/ext_filter.load"
+	notifies :reload, "service[apache2]", :delayed
+end
+=begin
+cookbook_file "/etc/fema_banner.html" do
+	source "fema_banner.html"
+	mode "0644"
+end
+=end
+
+remote_file "#{Chef::Config[:file_cache_path]}/wordpress.tar.gz" do
+	source "http://wordpress.org/latest.tar.gz"
+	if node.deployment.environment == 'dev'
+		notifies :run, "execute[install latest Wordpress]", :immediately
 	end
+end
 
+execute "install latest Wordpress" do
+	command "tar --strip-components=1 -xzf #{Chef::Config[:file_cache_path]}/wordpress.tar.gz"
+	cwd "/var/www/html"
+	not_if { ::File.exists?("/var/www/html/wp-config.php") }
+end
 
+# Install heartbeat for ELB
+template '/var/www/html/heartbeat.php' do
+owner 'root'
+group 'root'
+mode '0644'
+source "heartbeat.php.erb"
+end
 
-	service "httpd" do
-	  action :restart
-	end
+# Install .htaccess for redirects
+template '/var/www/html/.htaccess' do
+owner 'root'
+group 'root'
+mode '0644'
+source "wp-htaccess.erb"
+end
 
-when "debian"
+execute "chown -R apache:apache /var/www/html"
 
-	
+execute "setsebool -P httpd_can_network_connect on" do
+	not_if "getsebool httpd_can_network_connect | grep ' on$'"
+	notifies :reload, "service[apache2]", :delayed
+end
 
-	bash "Create mysql database in RDS" do
-		user "root"
-		code <<-EOH
-			mysql -h #{$db_host} -u #{$db_user} -p#{$db_password} -e "CREATE DATABASE IF NOT EXISTS #{$db_name};"
+[80, 443].each { |port|
+	bash "Allow #{port} through iptables" do
+	  user "root"
+	  not_if "/sbin/iptables -nL | egrep '^ACCEPT.*dpt:#{port}($| )'"
+	  code <<-EOH
+		  iptables -I INPUT -p tcp --dport #{port} -j ACCEPT
+	    service iptables save
+	    service iptables restart
 		EOH
 	end
+}
 
+#include_recipe 'chef-vault'
+#dbpass = chef_vault_item("wordpress", "dbpass")['password']
 
-	bash "Launch Wordpress site" do
-		user "root"
-		code <<-EOH
-			cd /var/www
-			mkdir -p wordpressapp
-			cd wordpressapp
-			wp core download
-			wp core config --dbname=#{$db_name} --dbuser=#{$db_user} --dbpass=#{$db_password} --dbhost=#{$db_host}
-			wp core multisite-install --url=#{$app_url} --title=#{$title} --admin_user=#{$admin_user}  --admin_password=#{$admin_password}  --admin_email=#{$admin_email}
-		EOH
-		not_if  {::File.exists?("/var/www/wordpressapp/wp-config.php") }
+template "/var/www/html/wp-config.php" do
+	source "wp-config.php.erb"
+	variables(
+		:dbname => $db_name,
+		:dbuser => $db_user,
+		:dbpass => $db_password,
+		:dbhost => $db_endpoint,
+		:dbport => 3306
+	)
+	mode 0644
+	sensitive true
+end
+
+=begin
+femadata_cert_auth_info = chef_vault_item("certs", ".femadata.crt")
+femadata_cert_key_auth_info = chef_vault_item("certs", ".femadata.key")
+
+file "/etc/httpd/ssl/femadata.crt" do
+	content femadata_cert_auth_info['file-content']
+	mode 0600
+	sensitive true
+	notifies :reload, "service[apache2]", :delayed
+end
+file "/etc/httpd/ssl/femadata.key" do
+	content femadata_cert_key_auth_info['file-content']
+	mode 0600
+	sensitive true
+	notifies :reload, "service[apache2]", :delayed
+end
+=end
+web_app "wordpress" do
+	server_name "www.cloudamatic.com"
+	server_aliases [ node.fqdn, node.hostname, node.hostname+".cloudamatic.com", $lb_url ]
+=begin
+	if node.deployment.environment == 'dev' or node.deployment.environment == 'development'
+		server_aliases [ node.fqdn, node.hostname, node.hostname+".cloudamatic.com", "www.dev.cloudamatic.com", node.ec2.public_ip_address ]
+	else
+		server_aliases [ node.fqdn, node.hostname, node.hostname+".cloudamatic.com", "cloudamatic.com", node.deployment.loadbalancers.wordpress.dns ]
 	end
+=end	#cookbook "femadata-mgmt"
+	docroot "/var/www/html"
+	allow_override "All"
+	template "wp-vhost.conf.erb"
+	notifies :reload, "service[apache2]", :delayed
+end
+=begin
+# Parts and pieces for our LIDAR data S3 browser
+remote_file "#{Chef::Config[:file_cache_path]}/ruby-2.1.rpm" do
+	action :create
+	source "https://s3.amazonaws.com/cap-public/ruby212-2.1.2p205-1.el6.x86_64.rpm"
+end
+
+execute "fix perms on Ruby" do
+	command "chmod -R 755 /opt/rubies ; chcon -R -h -t httpd_sys_content_t /opt/rubies"
+	action :nothing
+end
+
+rpm_package "Ruby RPM" do
+	source "#{Chef::Config[:file_cache_path]}/ruby-2.1.rpm"
+	notifies :run, "execute[fix perms on Ruby]", :immediately
+end
+
+gem_package "aws-sdk-core" do
+	gem_binary "/opt/rubies/ruby212-2.1.2-p205/bin/gem"
+	notifies :run, "execute[fix perms on Ruby]", :immediately
+end
+
+directory "/var/www/lidar" do
+	mode 0755
+end
+execute "chcon -h -t httpd_sys_content_t /var/www/lidar"
+
+cookbook_file "/var/www/lidar/index.rb" do
+  source "index.rb"
+	mode 0755
+end
+execute "chcon -h -t httpd_sys_content_t /var/www/lidar/index.rb"
 
 
-	template '/var/www/wordpressapp/heartbeat.php' do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source "wordpress/heartbeat.php.erb"
-	end
+web_app "lidar" do
+	server_name "lidar.femadata.com"
+	server_aliases [ "www.lidar.femadata.com" ]
+	directory_index [ "index.rb" ]
+	cookbook "femadata-mgmt"
+	docroot "/var/www/lidar"
+	allow_override "All"
+	template "lidar.conf.erb"
+	notifies :reload, "service[apache2]", :delayed
+end
 
-
-	template '/var/www/wordpressapp/.htaccess' do
-	owner 'root'
-	group 'root'
-	mode '0644'
-	source "wordpress/htaccess.erb"
-	end
-
-   
-	bash "Change owner and group" do
-		user "root"
-		code <<-EOH  
-			cd /var/www
-			chown -R apache:apache wordpressapp
-		EOH
-	end
-
-	service "apache2" do
-	  action :restart
-	end
-
-
-else
-
- raise '#{node[:platform_family]} not supported'
-
-end 
+if node.deployment.environment == 'dev' or node.deployment.environment == 'development'
+	include_recipe "femadata-ad::linux"
+end
+=end
