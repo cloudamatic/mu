fb67b6851a038b1f3b8734b9459559d866c28730
Ami Rahav <amiramr0@gmail.com>
2015-07-04 20:04:50 -0400

    Make a littlie bit more sense of mu-jenkins

--- cookbooks/mu-jenkins/attributes/default.rb
+++ cookbooks/mu-jenkins/attributes/default.rb
@@ -1,6 +1,8 @@
-default.jenkins_users = %w{testuser}
+default.jenkins_users = [
+	{:user_name => "testuser", :fullname => "testuser", :email => "testuser@email.com", :vault => "jenkins", :vault_item => "testuser"}
+]
+
 default.jenkins_ssh_urls = [node.ipaddress]
-default.jenkins_create_demo_job = false
 
 node.normal.java.oracle.accept_oracle_download_terms = true
 node.normal.java.java_home = '/usr/lib/jvm/java'
@@ -8,3 +10,11 @@ node.normal.java.install_flavor = 'oracle'
 node.normal.java.jdk_version = 8
 node.normal.java.jdk['8']["x86_64"]["url"] = "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
 node.normal.java.jdk["8"]["x86_64"]["checksum"] = "1ad9a5be748fb75b31cd3bd3aa339cac"
+
+default.jenkins_ssh_vault = {
+	:vault => "jenkins", :item => "ssh"
+}
+
+default.jenkins_admin_vault = {
+	:vault => "jenkins", :item => "admin"
+}
