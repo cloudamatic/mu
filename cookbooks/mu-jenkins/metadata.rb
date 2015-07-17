8a551b6e8ad39fb4905c30a0a42d9a16f2125557
rpattcorner <robert.patt-corner@eglobaltech.com>
2015-07-17 08:03:46 -0400

    bump version

--- cookbooks/mu-jenkins/metadata.rb
+++ cookbooks/mu-jenkins/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mu-developers@googlegroups.com'
 license          'All rights reserved'
 description      'Installs/Configures mu-jenkins'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 depends		 'java'
 depends		 'jenkins'
 depends		 'chef-vault'
