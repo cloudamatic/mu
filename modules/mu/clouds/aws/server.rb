8252ebe8f53723cae6c199e7430a9d9e1d79a058
Ami Rahav <amiramr0@gmail.com>
2016-06-30 16:58:45 -0400

    Fixe for existing_deploys

--- modules/mu/clouds/aws/server.rb
+++ modules/mu/clouds/aws/server.rb
@@ -959,7 +959,7 @@ module MU
                 ).first
 
                 MU.log "Couldn't find exisiting resource #{ext_deploy["cloud_id"]}, #{ext_deploy["cloud_type"]}", MU::ERR if found.nil?
-                @deploy.notify(ext_deploy["cloud_type"], found.deploy_id, found.notify, mu_name: found.mu_name, triggering_node: @mu_name)
+                @deploy.notify(ext_deploy["cloud_type"], found.config["name"], found.deploydata, mu_name: found.mu_name, triggering_node: @mu_name)
               elsif ext_deploy["mu_name"] && ext_deploy["deploy_id"]
                 MU.log "#{ext_deploy["mu_name"]} / #{ext_deploy["deploy_id"]}"
                 found = MU::MommaCat.findStray(
@@ -972,7 +972,7 @@ module MU
                 ).first
 
                 MU.log "Couldn't find exisiting resource #{ext_deploy["mu_name"]}/#{ext_deploy["deploy_id"]}, #{ext_deploy["cloud_type"]}", MU::ERR if found.nil?
-                @deploy.notify(ext_deploy["cloud_type"], ext_deploy["deploy_id"], found.notify, mu_name: ext_deploy["mu_name"], triggering_node: @mu_name)
+                @deploy.notify(ext_deploy["cloud_type"], found.config["name"], found.deploydata, mu_name: ext_deploy["mu_name"], triggering_node: @mu_name)
               else
                 MU.log "Trying to find existing deploy, but either the cloud_id is not valid or no mu_name and deploy_id where provided", MU::ERR
               end
