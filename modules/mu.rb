0bea053c3da69ed66d0753ce53e81ca0643e8e0e
Ami Rahav <amiramr0@gmail.com>
2016-08-21 11:43:06 -0400

    Chef and gem updates

--- modules/mu.rb
+++ modules/mu.rb
@@ -506,7 +506,7 @@ module MU
 
   # The version of Chef we will install on nodes (note- not the same as what
   # we intall on ourself, which comes from install/mu_setup).
-  @@chefVersion = "12.11.18-1"
+  @@chefVersion = "12.13.37-1"
   # The version of Chef we will install on nodes.
   # @return [String]
   def self.chefVersion;
