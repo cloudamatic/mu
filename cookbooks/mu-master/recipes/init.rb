e03587a7d391a159cd4836b0586c167f4256ba58
John Stange <john.stange@eglobaltech.com>
2017-05-03 18:42:34 -0400

    mu-master::init almost to knife bootstrap on its own

--- cookbooks/mu-master/recipes/init.rb
+++ cookbooks/mu-master/recipes/init.rb
@@ -15,9 +15,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'etc'
+
 CHEF_SERVER_VERSION="12.11.1-1"
 CHEF_CLIENT_VERSION="12.17.44-1"
 MU_BRANCH="its_all_your_vault"
+MU_BASE="/opt/mu"
+SSH_USER="root"
 
 execute "reconfigure Chef server" do
   command "/opt/opscode/bin/chef-server-ctl reconfigure"
@@ -30,7 +34,7 @@ rpms = {}
 dpkgs = {}
 
 if platform_family?("rhel") 
-  basepackages = ["git", "curl", "vim-enhanced", "zip", "unzip", "java-1.8.0-openjdk", "gcc", "gcc-c++", "make", "libxml2-devel", "libxslt-devel", "cryptsetup-luks", "python-pip", "lsof", "mlocate", "strace", "nmap", "openssl-devel", "readline-devel", "python-devel", "diffutils", "patch", "bind-utils", "httpd-tools", "mailx", "postgresql-devel", "openssl", "libyaml", "graphviz", "ImageMagick-devel", "graphviz-devel", "jq"]
+  basepackages = ["git", "curl", "vim-enhanced", "zip", "unzip", "java-1.8.0-openjdk", "gcc", "gcc-c++", "make", "libxml2-devel", "libxslt-devel", "cryptsetup-luks", "python-pip", "lsof", "mlocate", "strace", "nmap", "openssl-devel", "readline-devel", "python-devel", "diffutils", "patch", "bind-utils", "httpd-tools", "mailx", "postgresql-devel", "openssl", "libyaml", "graphviz", "ImageMagick-devel", "graphviz-devel", "jq", "vim"]
   rpms = {
     "epel-release" => "http://mirror.metrocast.net/fedora/epel/epel-release-latest-#{node[:platform_version].to_i}.noarch.rpm",
     "chef-server-core" => "https://packages.chef.io/stable/el/#{node[:platform_version].to_i}/chef-server-core-#{CHEF_SERVER_VERSION}.el#{node[:platform_version].to_i}.x86_64.rpm"
@@ -59,29 +63,48 @@ else
   raise "Mu Masters are currently only supported on RHEL-family hosts."
 end
 
+if File.read("/etc/ssh/sshd_config").match(/^AllowUser\s+([^\s]+)(?: |$)/)
+  SSH_USER=Regexp.last_match[1].chomp
+end
+
+
 package basepackages
 rpms.each_pair { |pkg, src|
   rpm_package pkg do
     source src
-    notifies :run, "execute[reconfigure Chef server]", :immediately if pkg == "chef-server-core"
+    notifies :run, "execute[reconfigure Chef server]", :immediately if pkg == "chef-server-core" and File.exists?("/opt/opscode/bin/chef-server-ctl")
   end
 }
 package removepackages do
   action :remove
 end
 
-["bin", "etc", "lib", "var"].each { |mudir|
-  directory "/opt/mu/#{mudir}" do
-    mode 0755
+["bin", "etc", "lib", "var/users/mu", "var/deployments", "var/orgs/mu"].each { |mudir|
+  directory "#{MU_BASE}/#{mudir}" do
+    mode mudir.match(/^var\//) ? 0700 : 0755
     owner "root"
     recursive true
   end
 }
 
-git "/opt/mu/lib" do
+git "#{MU_BASE}/lib" do
   repository "git://github.com/cloudamatic/mu.git"
   # XXX if we can check that we're not in chef-apply mode, use an attribute to pick our branch; otherwise use the default
-  revision "its_all_your_vault"
+  revision MU_BRANCH
+end
+
+["mu-aws-setup", "mu-cleanup", "mu-deploy", "mu-firewall-allow-clients", "mu-gen-docs", "mu-load-config.rb", "mu-load-murc.rb", "mu-momma-cat", "mu-node-manage", "mu-tunnel-nagios", "mu-upload-chef-artifacts", "mu-user-manage"].each { |exe|
+  link "#{MU_BASE}/bin/#{exe}" do
+    to "#{MU_BASE}/lib/bin/#{exe}"
+  end
+}
+remote_file "#{MU_BASE}/bin/mu-self-update" do
+  source "file://#{MU_BASE}/lib/bin/mu-self-update"
+  mode 0755
+end
+remote_file "#{MU_BASE}/bin/mu-configure" do
+  source "file://#{MU_BASE}/lib/install/mu_setup"
+  mode 0755
 end
 
 ["/usr/local/ruby-current/bin/gem", "/opt/chef/embedded/bin/gem", "/opt/opscode/embedded/bin/gem"].each { |gembin|
@@ -91,11 +114,78 @@ end
     package_name "bundler"
   end
   execute "#{bundler_path} install" do
-    cwd "/opt/mu/lib/modules"
+    cwd "#{MU_BASE}/lib/modules"
     umask 0022
     not_if "#{bundler_path} check"
   end
 }
+
+# Get a 'mu' Chef org in place and populate it with artifacts
+directory "/root/.chef"
+#remote_file "temporary root knife.rb for initial setup" do
+#  source "file:///etc/opscode/pivotal.rb"
+#  path "/root/.chef/knife.rb"
+#end
+execute "initial Chef artifact upload" do
+  command "CHEF_PUBLIC_IP=127.0.0.1 MU_INSTALLDIR=#{MU_BASE} MU_LIBDIR=#{MU_BASE}/lib MU_DATADIR=#{MU_BASE}/var #{MU_BASE}/lib/bin/mu-upload-chef-artifacts"
+  action :nothing
+end
+gem "simple-password-gen" do
+  compile_time true
+end
+require "simple-password-gen"
+# XXX this would make an awesome library
+execute "create mu Chef user" do
+  command "/opt/opscode/bin/chef-server-ctl user-create mu Mu Master root@example.com #{Password.pronounceable} -f #{MU_BASE}/var/users/mu/mu.user.key"
+  umask 0277
+  not_if "/opt/opscode/bin/chef-server-ctl user-list | grep '^mu$'"
+end
+execute "create mu Chef org" do
+  command "/opt/opscode/bin/chef-server-ctl org-create mu mu -a mu -f #{MU_BASE}/var/orgs/mu/mu.org.key"
+  umask 0277
+  not_if "/opt/opscode/bin/chef-server-ctl org-list | grep '^mu$'"
+end
+file "initial root knife.rb" do
+  path "/root/.chef/knife.rb"
+  content "
+  node_name 'mu'
+  client_key '#{MU_BASE}/var/users/mu/mu.user.key'
+  validation_client_name 'mu-validator'
+  validation_key '#{MU_BASE}/var/orgs/mu/mu.org.key'
+  chef_server_url 'https://127.0.0.1/organizations/mu'
+  chef_server_root 'https://127.0.0.1/organizations/mu'
+  syntax_check_cache_path  '/root/.chef/syntax_check_cache'
+  cookbook_path [ '/root/.chef/cookbooks', '/root/.chef/site_cookbooks' ]
+  ssl_verify_mode :verify_none
+  knife[:vault_mode] = 'client'
+  knife[:vault_admins] = ['mu']\n"
+  only_if { !::File.exists?("/root/.chef/knife.rb") }
+  notifies :run, "execute[initial Chef artifact upload]", :immediately
+end
+
+
+# Rig us up for a knife bootstrap
+SSH_DIR="#{Etc.getpwnam(SSH_USER).dir}/.ssh"
+directory SSH_DIR do
+  mode 0700
+end
+bash "add localhost ssh to authorized_keys and config" do
+  code <<-EOH
+    cat #{SSH_DIR}/id_rsa.pub >> #{SSH_DIR}/authorized_keys
+    echo "Host localhost" >> #{SSH_DIR}/config
+    echo "  IdentityFile #{SSH_DIR}/id_rsa" >> #{SSH_DIR}/config
+  EOH
+  action :nothing
+end
+execute "ssh-keygen -N '' -f #{SSH_DIR}/id_rsa" do
+  umask 0177
+  not_if { ::File.exists?("#{SSH_DIR}/id_rsa") }
+  notifies :run, "bash[add localhost ssh to authorized_keys and config]", :immediately
+end
+execute "create MU-MASTER Chef client" do
+  command "/opt/chef/bin/knife bootstrap -N MU-MASTER --no-node-verify-api-cert --node-ssl-verify-mode=none 127.0.0.1"
+  not_if "/opt/chef/bin/knife node list | grep '^MU-MASTER$'"
+end
 
 #remote_file "#{Chef::Config[:file_cache_path]}/vault-#{node[:mu][:vault][:version]}.zip" do
 #  source "https://releases.hashicorp.com/vault/#{node[:mu][:vault][:version]}/vault_#{node[:mu][:vault][:version]}_linux_amd64.zip"
