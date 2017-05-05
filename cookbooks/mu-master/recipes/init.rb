6aea947f08812dad1cc5332767acdbd94040d805
John Stange <john.stange@eglobaltech.com>
2017-05-05 17:35:41 -0400

    make sure there's a ruby in the path for berks

--- cookbooks/mu-master/recipes/init.rb
+++ cookbooks/mu-master/recipes/init.rb
@@ -151,8 +151,12 @@ directory "/root/.chef"
 execute "knife ssl fetch" do
   action :nothing
 end
+link "/usr/bin/ruby" do
+  to "/usr/local/ruby-current/bin/ruby"
+  not_if{ ::File.exists?("/usr/bin/ruby") }
+end
 execute "initial Chef artifact upload" do
-  command "PATH=\"/usr/local/ruby-current/bin:${PATH}\" CHEF_PUBLIC_IP=127.0.0.1 MU_INSTALLDIR=#{MU_BASE} MU_LIBDIR=#{MU_BASE}/lib MU_DATADIR=#{MU_BASE}/var #{MU_BASE}/lib/bin/mu-upload-chef-artifacts"
+  command "CHEF_PUBLIC_IP=127.0.0.1 MU_INSTALLDIR=#{MU_BASE} MU_LIBDIR=#{MU_BASE}/lib MU_DATADIR=#{MU_BASE}/var #{MU_BASE}/lib/bin/mu-upload-chef-artifacts"
   action :nothing
   notifies :run, "execute[knife ssl fetch]", :before
 end
