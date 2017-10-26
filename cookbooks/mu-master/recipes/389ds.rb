61c7306c9f68700e82228aacd2edd67c7772c4ea
Mu Master <femateam@eglobaltech.com>
2017-10-26 13:18:31 -0400

    389ds setup script bug workaroud

--- cookbooks/mu-master/recipes/389ds.rb
+++ cookbooks/mu-master/recipes/389ds.rb
@@ -68,6 +68,7 @@ $CREDS.each_pair { |creds, cfg|
 #  %x{/usr/sbin/setenforce 0}
 execute "initialize 389 Directory Services" do
   command "/usr/sbin/setup-ds-admin.pl -s -f /root/389ds.tmp/389-directory-setup.inf --continue --debug #{Dir.exists?("/etc/dirsrv/slapd-#{$MU_CFG["hostname"]}") ? "--update" : ""}"
+  ignore_failure true # XXX this is a bug in the package; we're currently getting spurious exit(1)s from this thing even when it works fine
   action :nothing
 end
 
