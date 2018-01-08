8e5f377209fd82233f14424814c0da07b8c35f53
agill17 <root@example.com>
2018-01-08 23:41:31 +0000

    Deleted extra dir, inspec execution for async_groom

--- test/exec_inspec.py
+++ test/exec_inspec.py
@@ -12,13 +12,12 @@ import ast
 
 deploy_dirs = '/opt/mu/var/deployments'
 current_deploys = os.listdir(deploy_dirs)
-workspace = os.environ['WORKSPACE']
+workspace = '/home/jenkins/workspace/test2'
 test = workspace+'/test'
 
 
 def get_profile():
   which_profile = str(sys.argv[1])
-  print which_profile
   if os.path.isdir(test+'/'+which_profile):
     return which_profile
   else:
@@ -34,6 +33,7 @@ def get_deploy_id(bok, all_boks=workspace+'/demo'):
     for each in line:
       stripped =  each.strip()
       if 'appname:' in stripped:
+        print stripped
         partial_dep_name = stripped.split()[1].upper()+'-DEV'
         break
 
@@ -44,25 +44,31 @@ def get_deploy_id(bok, all_boks=workspace+'/demo'):
   return deploy_id
 
 
-
-### Use only for async_groom
-def wait_till_confirmed(seconds_to_wait):
-  done = False
-  file_to_check = '/Mu_Logs/master.log'
-  while done == False:
-    file1 = os.stat(file_to_check) # initial file size
-    start_size = file1.st_size
-    time.sleep(seconds_to_wait)
-    file2 = os.stat(file_to_check) # updated file size
-    new_size = file2.st_size
-    comp = new_size - start_size # compares sizes
-    if comp != 0:
-      print 'INFO: File changes detected.. Checking again after %s seconds....' % str(seconds_to_wait)
-      done = False
+def wait_for_servers(deploy_id,seconds_to_poll=3):
+  node = json.load(open(deploy_dirs+'/'+deploy_id+'/deployment.json')) 
+  servers_present = False
+  
+  while servers_present == False:
+    if node.has_key('servers'):
+      servers_present = True
     else:
-      done = True
-      print 'SKIP: No new changes detected in the logs, continuing to inspec...'
+      time.sleep(seconds_to_poll)
+      servers_present = False
+      print "INFO: Servers not found. Checking back in %s seconds" % seconds_to_poll
 
+
+def wait_till_confirmed(deploy_id, node, seconds_to_poll):
+  done = False
+  os.chdir(deploy_dirs+'/'+deploy_id)
+  while done == False:
+    if os.path.isfile(node+'_done.txt'):
+      print "INFO: Server Converged: %s" % node
+      done = True
+    else:
+      print "INFO: Server =====> %s <===== not fully converged. Checking back in %s seconds." % (node, seconds_to_poll)
+      done = False
+      time.sleep(10)  
+  return done
 
 def get_load_balancers(deploy_id):
   all_loads = []
@@ -83,98 +89,118 @@ def get_load_balancers(deploy_id):
 def get_host_info(deploy_id):
   host_infos = []
   ssh_info = {}
+  print deploy_id
   if os.path.isdir(deploy_dirs+'/'+deploy_id):
     node = json.load(open(deploy_dirs+'/'+deploy_id+'/deployment.json'))
     bok = json.load(open(deploy_dirs+'/'+deploy_id+'/basket_of_kittens.json'))
     platform=None
     ssh_user=None
     fqdn = None
-    ### detect if servers or server_pools?
-    if bok.get('servers') != None:
+    server_pools = True
+      
+    try:
       platform = bok['servers'][0]['platform']
       ssh_user = bok['servers'][0]['ssh_user']
-
-    elif bok.get('server_pools') != None:
+    except KeyError as e:
       platform = bok['server_pools'][0]['platform']
       ssh_user = bok['server_pools'][0]['ssh_user']
 
     load_balancers = get_load_balancers(deploy_id)
-
+    
+    ### Get SSH Key Name from deploy_id dir
+    key_file = open(deploy_dirs+'/'+deploy_id+'/ssh_key_name')
+    ssh_key = key_file.readline().strip()
+    
     for k,v in node['servers'].iteritems():
       control = []
       run_list = []
       dep = None
+  
+      ### need each servers ssh info
       for key,val in v.iteritems():
         if key.startswith(deploy_id+'-'+k.upper()):
           dep = key
+          print 'INFO: Deploy ID Detected: %s' % dep
           run_list = val['run_list']
-          
+
           ### Get DNS
           if len(v[dep]['public_dns_name']) != 0:
             fqdn = v[dep]['public_dns_name']
           else:
             fqdn = v[dep]['private_dns_name']
           
+          ### Filter recipe names to map to inspec controls
           for recipe in run_list:
-            if '::' in recipe:
+            if '::' in recipe \
+            and 'store_attr' not in recipe \
+            and 'default' not in recipe and 'nat' not in recipe:
               recipe_name = re.search(r"\w+]", recipe).group(0).replace(']','')
-              if recipe_name != 'store_attr' and recipe_name != 'default':
-                control.insert(0,recipe_name)
-              else:
-                print 'SKIP: Not adding ==> %s <== to the controls list' % recipe_name
-                run_list.remove(recipe)
-      
-      ### Get SSH Key Name from deploy_id dir
-      key_file = open(deploy_dirs+'/'+deploy_id+'/ssh_key_name')
-      ssh_key = key_file.readline().strip()
-      
+              control.insert(0,recipe_name)
       
       ssh_info = {'server_name': k, 'fqdn': fqdn, 'ssh_user':ssh_user, 'ssh_file': '~/.ssh/'+ssh_key, 'controls': control, 'platform': platform, 'load_balancers':load_balancers } 
       host_infos.append(ssh_info)
-      #print ssh_info
+    
+  
   else:
     raise Exception('ERROR: '+deploy_id+' ==> deploy id does not exist in '+deploy_dirs)
   return host_infos
- 
+
+
+
 def store_ssh_info(array_of_ssh_info, where='/tmp'):
   for ssh_info in array_of_ssh_info:
     print ssh_info
     ya = open(where+'/'+ssh_info['server_name']+'_attr.yaml','w')
+    ### yes safe_dump to get rid of python unicode text
     yaml.safe_dump(ssh_info, ya,default_flow_style=False)
 
 
 
-#########################################################################
-##### Run Tests
+def get_win_pass(deploy_id):
+  command = subprocess.check_output(['knife', 'vault', 'show', deploy_id+'-WINDOWS','windows_credentials'])
+  for line in command.splitlines():
+    if line.startswith('password:'):
+      return line.split(':')[1].strip()
+      break
+
+
+
+def run_windows_tests(deploy_id,profile,controls,ssh_user):
+  pas =  "\'%s\'" % get_win_pass(deploy_id)
+  winrm = "winrm://%s@%s" % (ssh_user,deploy_id.upper()+'-WINDOWS')
+  cmd = "inspec exec "+profile+" --controls="+controls+" -t "+winrm+" --password "+pas
+
+
+def run_linux_tests(profile, ssh, ssh_file, all_controls):
+  status = subprocess.call(['inspec','exec', profile, '--controls='+all_controls,'-t',ssh, '-i', ssh_file])
+  return status
+  
+
+
 bok_name = None
 if str(sys.argv[2]) != None:
   bok_name = str(sys.argv[2])
-  print bok_name
 
 profile = get_profile()
-
-####### FOR ETCO ONLY -- ASYNC_GROOM
-if 'ETCO' in bok_name or 'etco' in bok_name:
-  wait_till_confirmed(70)
-###################
-
 deploy_id = get_deploy_id(bok_name)
 ssh_infos = get_host_info(deploy_id)
-os.chdir(test)
 store_ssh_info(ssh_infos)
-
+#########################################################################
+##### Run Tests ###########################################
 for ssh_info in ssh_infos:
+
+  ### get all inspec controls to run
+  controls = ssh_info['controls']
+  all_controls_spaced_out = ' '.join(controls)
+  print 'Control =====> '+all_controls_spaced_out+' <=====' 
   
+  ### Check if servers are up and  grooming finished
+  wait_for_servers(deploy_id)
+  wait_till_confirmed(deploy_id, ssh_info['server_name'],5)
+ 
+  os.chdir(test)
   if ssh_info['platform'] == 'windows':
-    print 'winrm is not yet configured...'
-    ## Figure out how to perform winrm here...
+    exit_status = run_windows_tests(deploy_id, profile, all_controls_spaced_out, ssh_info['ssh_user'])
   else:
     ssh = 'ssh://%s@%s' % (ssh_info['ssh_user'], ssh_info['fqdn'])
-    controls = ssh_info['controls']
-    all_controls_spaced_out = ' '.join(controls)
-    print 'Control'+'='*5+'>'+all_controls_spaced_out+'<'+5*"="
-    
-    ssh = 'ssh://%s@%s' % (ssh_info['ssh_user'], ssh_info['fqdn'])
-    exit_status = subprocess.call(['inspec','exec', profile, '--controls='+all_controls_spaced_out,'-t',ssh, '-i',ssh_info['ssh_file']]) 
-    if int(exit_status) != 0:
-      raise Exception("Tests Failed with Exit status: "+str(exit_status))
+    exit_status = run_linux_tests(profile, ssh, ssh_info['ssh_file'],all_controls_spaced_out)
