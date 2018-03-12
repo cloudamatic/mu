ba9c01fa88028cc8289db3766f2c947680034ad6
Mu Master <robert.patt-corner@eglobaltech.com>
2018-03-12 13:54:17 -0400

    first working simple-s-php

--- test/simple-server-php-test/controls/apachephp.rb
+++ test/simple-server-php-test/controls/apachephp.rb
@@ -1,16 +1,18 @@
+include_controls 'mu-tools-test'
 node =json('/tmp/chef_node.json').params
 control 'apache' do
     title 'This will test apache2 recipe'
-	%w(apache2).each do |pack|
-    describe package(pack) do
-      it { should be_installed }
+	  %w(apache2).each do |pack|
+      describe package(pack) do
+        it { should be_installed }
+      end
     end
 
   	describe service('apache2') do
 	    it { should be_installed }
 	    it { should be_enabled }
 	    it { should be_running }
-  end
+    end
 end
 
 control 'php' do 
@@ -19,4 +21,5 @@ control 'php' do
     describe package(pack) do
       it { should be_installed }
     end
+  end
 end
