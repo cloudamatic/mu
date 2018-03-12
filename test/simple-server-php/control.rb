a30f04ccf2d29bc7428ac653fd3832c44e5d44e3
rpattcorner <robert.patt-corner@eglobaltech.com>
2018-03-12 13:23:52 -0400

    simple-server-php
    Initial test

--- test/simple-server-php/control.rb
+++ test/simple-server-php/control.rb
@@ -1,10 +1,22 @@
 node =json('/tmp/chef_node.json').params
 control 'apache' do
     title 'This will test apache2 recipe'
-        # inspec-resources to test apache2 recipe
+	%w(apache2).each do |pack|
+    describe package(pack) do
+      it { should be_installed }
+    end
+
+  	describe service('apache2') do
+	    it { should be_installed }
+	    it { should be_enabled }
+	    it { should be_running }
+  end
 end
 
 control 'php' do 
   title 'This will test the php recipe'
-  # do something
+	%w(php).each do |pack|
+    describe package(pack) do
+      it { should be_installed }
+    end
 end
