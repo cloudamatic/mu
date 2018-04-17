3c4dc7a1824117400a3b3bf06b3b6fd05ea24a37
Mu Administrator <root@example.com>
2018-04-17 20:34:33 +0000

    update Gemfile and Gemfile.lock

--- cookbooks/mu-master/recipes/init.rb
+++ cookbooks/mu-master/recipes/init.rb
@@ -31,7 +31,7 @@ require 'socket'
 CHEF_SERVER_VERSION="12.17.15-1"
 CHEF_CLIENT_VERSION="14.0.190"
 KNIFE_WINDOWS="1.9.0"
-MU_BRANCH="the_goog"
+MU_BRANCH="chef_14_support"
 MU_BASE="/opt/mu"
 
 begin
