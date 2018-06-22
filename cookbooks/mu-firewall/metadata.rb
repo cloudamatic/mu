9e62bce4f58e567fb5992b630c1f1830a6a22f34
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2018-06-22 14:47:43 -0400

    Fix FC008

--- cookbooks/mu-firewall/metadata.rb
+++ cookbooks/mu-firewall/metadata.rb
@@ -1,7 +1,20 @@
 name             'mu-firewall'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Mu'
+maintainer_email 'mu-developers@googlegroups.com'
+license          '# Copyright:: Copyright (c) 2014 eGlobalTech, Inc., all rights reserved
+#
+# Licensed under the BSD-3 license (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the root of the project or at
+#
+#     http://egt-labs.com/mu/LICENSE.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.'
+#'
 description      'Installs/Configures mu-firewall'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
