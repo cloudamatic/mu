826de3c65fde3d980b40bc5d83d585dfc46d1481
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-03-01 14:12:58 -0500

    restructure get_metadata method

--- modules/mu/clouds/azure.rb
+++ modules/mu/clouds/azure.rb
@@ -130,24 +130,14 @@ module MU
         begin
           response = nil
           Timeout.timeout(1) do
-            response = open("#{base_url}/?api-version=#{ api_version }","Metadata"=>"true").read
-            #JSONresponse = MultiJson.load(response)
+            response = JSON.parse(open("#{base_url}/?api-version=#{ api_version }","Metadata"=>"true").read)
           end
 
           response
         rescue
-          pp response
+          MU.log "Failed to get Azure MetaData."
         end
       end
     end
   end
 end
-
-
-
-require 'open-uri'
-require 'json'
-
-url_metadata="http://169.254.169.254/metadata/instance?api-version=2017-04-02"
-
-puts open(url_metadata,"Metadata"=>"true").read
