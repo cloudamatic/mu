b2401a0f69ba972bed7abc72a6526a2db1fbaaf5
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-03 16:35:21 -0400

    install iptables and iptables-services early.

--- cookbooks/mu-firewall/recipes/default.rb
+++ cookbooks/mu-firewall/recipes/default.rb
@@ -7,4 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+if ['rhel', 'amazon'].include? node['platform_version']
+    package ['iptables', 'iptables-services']  do
+		action :install
+		only_if node['firewall']['redhat7_iptables']
+	end
+end
+
 include_recipe 'firewall'
