ac8d8c60f100eb8767828aa77287c60e92ed3c76
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-10 16:35:27 -0400

    fix typo in expression

--- cookbooks/mu-master/metadata.rb
+++ cookbooks/mu-master/metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url 'https://github.com/cloudamatic/mu'
 issues_url 'https://github.com/cloudamatic/mu/issues'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-version '0.9.4'
+version '0.9.5'
 
 %w( centos amazon rhel ).each do |os|
 	supports os
