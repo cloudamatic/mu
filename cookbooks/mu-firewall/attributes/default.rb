376f44202d647266a1eb64bd6d2eaec1d185627d
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-10 12:15:22 -0400

    add allow loopback in firewall

--- cookbooks/mu-firewall/attributes/default.rb
+++ cookbooks/mu-firewall/attributes/default.rb
@@ -1,3 +1,4 @@
 default['firewall']['allow_ssh'] = true
 default['firewall']['firewalld']['permanent'] = true
 default['firewall']['ipv6_enabled'] = false
+default['firewall']['allow_loopback'] = true
