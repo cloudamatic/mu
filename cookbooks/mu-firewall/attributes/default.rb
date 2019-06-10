42228f0a74bed3cb4ac1ff9d5268660b39268d49
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-10 12:36:04 -0400

    add allow_established

--- cookbooks/mu-firewall/attributes/default.rb
+++ cookbooks/mu-firewall/attributes/default.rb
@@ -2,3 +2,4 @@ default['firewall']['allow_ssh'] = true
 default['firewall']['firewalld']['permanent'] = true
 default['firewall']['ipv6_enabled'] = false
 default['firewall']['allow_loopback'] = true
+default['firewall']['allow_established'] = true
