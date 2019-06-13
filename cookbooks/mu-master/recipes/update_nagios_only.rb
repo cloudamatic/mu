6371543ecde98282e2cf8c565220f5411a976418
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-13 12:16:25 -0400

    use the mu-nagios cookbook

--- cookbooks/mu-master/recipes/update_nagios_only.rb
+++ cookbooks/mu-master/recipes/update_nagios_only.rb
@@ -16,8 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe "nagios::server_source"
-include_recipe "nagios"
+include_recipe "mu-nagios::server_source"
+include_recipe "mu-nagios"
 include_recipe 'mu-master::firewall-holes'
 
 if $MU_CFG.has_key?('ldap')
@@ -49,7 +49,7 @@ file "/etc/sysconfig/nagios" do
   content "checkconfig=\"false\"\n"
   mode 0600
 end
-include_recipe "nagios"
+include_recipe "mu-nagios"
 
 # scrub our old stuff if it's around
 ["nagios_fifo", "nagios_more_selinux"].each { |policy|
