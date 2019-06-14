f7393bc21fd8696224b9315cd6506c26ef920925
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-14 16:04:30 -0400

    don't install the mysql plugin on AzL

--- cookbooks/mu-master/recipes/default.rb
+++ cookbooks/mu-master/recipes/default.rb
@@ -128,7 +128,15 @@ end
 include_recipe "mu-master::update_nagios_only"
 
 if !node['update_nagios_only']
-  package "nagios-plugins-all"
+
+  package %w(nagios-plugins-breeze nagios-plugins-by_ssh nagios-plugins-cluster nagios-plugins-dhcp nagios-plugins-dig nagios-plugins-disk nagios-plugins-disk_smb nagios-plugins-dns nagios-plugins-dummy nagios-plugins-file_age nagios-plugins-flexlm nagios-plugins-fping nagios-plugins-game nagios-plugins-hpjd nagios-plugins-http nagios-plugins-icmp nagios-plugins-ide_smart nagios-plugins-ircd nagios-plugins-ldap nagios-plugins-load nagios-plugins-log nagios-plugins-mailq nagios-plugins-mrtg nagios-plugins-mrtgtraf nagios-plugins-nagios nagios-plugins-nt nagios-plugins-ntp nagios-plugins-ntp-perl nagios-plugins-nwstat nagios-plugins-oracle nagios-plugins-overcr nagios-plugins-pgsql nagios-plugins-ping nagios-plugins-procs nagios-plugins-real nagios-plugins-rpc nagios-plugins-sensors nagios-plugins-smtp nagios-plugins-snmp nagios-plugins-ssh nagios-plugins-swap nagios-plugins-tcp nagios-plugins-time nagios-plugins-ups nagios-plugins-users nagios-plugins-wave) do
+    action :install
+  end
+
+  package %w(nagios-plugins-mysql) do
+      action :install
+      not_if { node['platform'] == 'amazon' }
+  end
 
   directory "/home/nagios" do
     owner "nagios"
