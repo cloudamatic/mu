d0e53b4eac724e927db9462faef0b3e61c7c13e6
Ryan Bolyard <ryan.bolyard@eglobaltech.com>
2019-06-24 13:29:16 -0400

    add reboot to selinux

--- cookbooks/mu-tools/recipes/selinux.rb
+++ cookbooks/mu-tools/recipes/selinux.rb
@@ -5,5 +5,12 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 selinux_state "SELinux Enforcing" do
-    action :enforcing
+	action :enforcing
+	notifies :reboot_now, 'reboot[now]', :immediately
 end
+
+reboot 'now' do
+	action :nothing
+	reason 'Must reboot to enable SELinux.'
+end
+  
