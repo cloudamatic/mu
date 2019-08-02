f99882406306cd738a4d35de9e357bf2c680115d
John Stange <john.stange@eglobaltech.com>
2019-08-02 15:39:26 -0400

    bump chef client version

--- cookbooks/mu-master/recipes/init.rb
+++ cookbooks/mu-master/recipes/init.rb
@@ -36,7 +36,7 @@ ENV['PATH'] = ENV['PATH']+":/bin:/opt/opscode/embedded/bin"
 # XXX We want to be able to override these things when invoked from chef-apply,
 # but, like, how?
 CHEF_SERVER_VERSION="12.17.15-1"
-CHEF_CLIENT_VERSION="14.11.21"
+CHEF_CLIENT_VERSION="14.13.11"
 KNIFE_WINDOWS="1.9.0"
 MU_BASE="/opt/mu"
 MU_BRANCH="master" # GIT HOOK EDITABLE DO NOT TOUCH
