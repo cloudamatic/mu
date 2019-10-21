5cfc83f841f97525c63531813c6b7c131fda41b8
John Stange <john.stange@eglobaltech.com>
2019-10-21 11:45:36 -0400

    AWS::ContainerCluster: move serviced-linked role creation for ELBs out of Chef and into AWS layer

--- cookbooks/mu-master/recipes/kubectl.rb
+++ cookbooks/mu-master/recipes/kubectl.rb
@@ -33,9 +33,3 @@ remote_file "/opt/mu/bin/aws-iam-authenticator" do
   mode 0755
   not_if "test -f /opt/mu/bin/aws-iam-authenticator"
 end
-
-# in brand new accounts where no load balancer has been created, something
-# has to do this before EKS has to, because by default it can't
-execute "PATH=\"/usr/local/python-current/bin:${PATH}\" aws iam create-service-linked-role --aws-service-name 'elasticloadbalancing.amazonaws.com'" do
-  not_if "PATH=\"/usr/local/python-current/bin:${PATH}\" aws iam list-roles | grep /aws-service-role/elasticloadbalancing.amazonaws.com/"
-end
