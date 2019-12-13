f6bb0f220125caf5edaaee61c9ad8e846ac2e435
John Stange <john.stange@eglobaltech.com>
2019-12-13 09:57:41 -0500

    Logger: don't try to write to deploy_dir logs after deploy_dir is gone

--- modules/mu/logger.rb
+++ modules/mu/logger.rb
@@ -125,7 +125,7 @@ module MU
 
       @@log_semaphere.synchronize {
         handles = [handle]
-        extra_logfile = if deploy and deploy.deploy_dir
+        extra_logfile = if deploy and deploy.deploy_dir and Dir.exist?(deploy.deploy_dir)
           File.open(deploy.deploy_dir+"/deploy.log", "a")
         end
         handles << extra_logfile if extra_logfile
