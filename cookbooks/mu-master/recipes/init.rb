c9516c038895f713e4117437d1516e9f5eca6901
John Stange <john.stange@eglobaltech.com>
2021-02-12 15:42:07 +0000

    semanage not in base packages, how droll

--- cookbooks/mu-master/recipes/init.rb
+++ cookbooks/mu-master/recipes/init.rb
@@ -205,7 +205,7 @@ when 'rhel'
     removepackages = ["nagios"]
 
   when 7
-    basepackages.concat(['libX11', 'mariadb-devel', 'cryptsetup', 'tcl-devel', 'gdbm-devel', 'sqlite-devel', 'tk-devel', 'perl-CGI', 'perl-DBI', 'perl-Data-Dumper', 'perl-Digest-MD5', 'perl-Git-SVN', 'perl-YAML', 'nvme-cli'])
+    basepackages.concat(['policycoreutils-python', 'libX11', 'mariadb-devel', 'cryptsetup', 'tcl-devel', 'gdbm-devel', 'sqlite-devel', 'tk-devel', 'perl-CGI', 'perl-DBI', 'perl-Data-Dumper', 'perl-Digest-MD5', 'perl-Git-SVN', 'perl-YAML', 'nvme-cli'])
     removepackages = ['nagios', 'firewalld']
 
   when 8
