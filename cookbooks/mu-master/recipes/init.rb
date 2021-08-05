c329ea12c91c4844e7c9073ba5ea6dcc1efe84d4
Mu Administrator <john.stange@eglobaltech.com>
2021-08-05 19:49:19 +0000

    Chef: still busted

--- cookbooks/mu-master/recipes/init.rb
+++ cookbooks/mu-master/recipes/init.rb
@@ -36,7 +36,7 @@ ENV['PATH'] = ENV['PATH']+":/bin:/opt/opscode/embedded/bin"
 # XXX We want to be able to override these things when invoked from chef-apply,
 # but, like, how?
 CHEF_SERVER_VERSION="14.0.65-1"
-CHEF_CLIENT_VERSION="16.9.29"
+CHEF_CLIENT_VERSION="16.14.1"
 KNIFE_WINDOWS="1.9.0"
 MU_BASE="/opt/mu"
 
