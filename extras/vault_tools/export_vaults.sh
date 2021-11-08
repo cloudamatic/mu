c3a895023b9f3521f4ae7dd090c7aeea4eeeddd2
Mu Administrator <icras-support@eglobaltech.com>
2021-11-08 23:56:21 +0000

    extras: export_vaults.sh a little smart in which vaults it skips

--- extras/vault_tools/export_vaults.sh
+++ extras/vault_tools/export_vaults.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Exports existing vaults to a vaults directory for use by test_vaults and recreate_vaults
-mkdir -p ~/vaults ; for i in `knife data bag list | grep -v -- -`;do echo $i; mkdir -p vaults/$i ; for j in `knife data bag show $i | grep -v '_keys$'`;do echo "   $j"; knife vault show $i $j -F json > vaults/$i/$j.json;done;done ; find vaults -empty -delete
+mkdir -p ~/vaults ; for i in `knife data bag list | grep -vE -- '-[0-9]{10}-'`;do echo $i; mkdir -p vaults/$i ; for j in `knife data bag show $i | grep -v '_keys$'`;do echo "   $j"; knife vault show $i $j -F json > vaults/$i/$j.json;done;done ; find vaults -empty -delete
